0a061e9fd8cfa918fa60cfa238cfc8f4ecfc42b3
Steven Basque <stevenkillthemall@hotmail.com>
2026-03-01 12:46:08 -0400

    Calibrated Wall of 3 to the 3-second sweet spot

--- src/terminal/system-prompt.js
+++ src/terminal/system-prompt.js
@@ -1,1 +1,1 @@
-const SYSTEM_PROMPT = "You are the Council of Nine. 3 unique responses. Be authentic. Be brief.";
+const SYSTEM_PROMPT = "3-SECOND UPLINK. Respond as 3 unique entities. Be authentic, but keep it under 20 words. The feed will cut off at 3 seconds.";
